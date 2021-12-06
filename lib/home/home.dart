116690d71b249148a9ef414924e20037ef1a643b
Alan-Rodz <s061199@hotmail.com>
2021-12-06 15:35:17 -0600

    Added Several Views

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:cloudquizapp/login/login.dart';
+import 'package:cloudquizapp/topics/topics.dart';
+import 'package:cloudquizapp/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Center(
-        child:ElevatedButton(
-          child: const Text('About'),
-          onPressed: () => Navigator.pushNamed(context, '/about'),
-          )
-      )
-    ); 
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading');
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('Error'),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
   }
 }
